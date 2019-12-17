99c274833e9b78167c3a4f6a28422bdbd65e6a77
Ana Marija Okorn <anamarija.okorn@gmail.com>
2019-12-17 09:05:53 +0100

    napisane tabele

--- uvoz/mojuvoz.R
+++ uvoz/mojuvoz.R
@@ -1,11 +1,22 @@
 library(readr)
-
 library(tidyr)
-
 library(dplyr)
+
 sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
 
 
-tabela1 <- read_csv2("podatki/st_dijakov_po_regijah.csv", skip = 3, col_names = c("Regija", "2009/2010", "2010/2011", "2011/2012", "2012/2013", "2013/2014", "2014/2015", "2015/2016", "2016/2017", "2017/2018", "2018/2019"),
+tabela1 <- read_csv2("podatki/st_dijakov_po_regijah.csv", skip = 3, col_names = c("Regija", "Leto", "Število dijakov"),
                   locale=locale(encoding="Windows-1250"))
+
+tabela2 <- read_csv2("podatki/st_dijakov_po_spolu_vrsti_izobrazevanja.csv", skip = 1, col_names = c("Vrsta izobraževanja", "Letnik", "Starost", "Leto", "Število moških", "Število žensk"),
+                     locale=locale(encoding="Windows-1250"))
+
+tabela3 <- read_csv2("podatki/st_diplomantov_po_regijah.csv", skip = 1, col_names = c("Regija", "Leto", "Število diplomantov"),
+                     locale=locale(encoding="Windows-1250"))
+
+tabela4 <- read_csv2("podatki/st_diplomantov_po_vrsti_izobraževanja_spolu.csv", skip = 1, col_names = c("Vrsta izobraževanja", "Leto", "Število moških", "Število žensk"),
+                     locale=locale(encoding="Windows-1250"))
+
+tabela5 <- read_csv2("podatki/st_studentov_po_vrsti_izobrazevanja_nacinu_studija.csv", skip = 1, col_names = c("Vrsta izobraževanja", "Leto", "Redni študij", "Izredni študij"),
+                     locale=locale(encoding="Windows-1250"))
