b5d5e6d9bfb855e9a119a90259e9e34ad7ff5105
Anamarija Okorn <anamarija.okorn@gmail.com>
2019-12-18 22:44:19 +0100

    združevanje 1 in 3 tabele

--- uvoz/mojuvoz.R
+++ uvoz/mojuvoz.R
@@ -10,7 +10,7 @@ tabela1 <- read_csv2("podatki/st_dijakov_po_regijah.csv", skip = 1, col_names = 
                   locale=locale(encoding="Windows-1250"))
 
 tabela2 <- read_csv2("podatki/st_dijakov_po_spolu_vrsti_izobrazevanja.csv", skip = 1, col_names = c("Vrsta izobraževanja", "Letnik", "Starost", "Leto", "Število moških", "Število žensk"),
-                     locale=locale(encoding="Windows-1250")) %>% select(c(-2, -3))
+                     locale=locale(encoding="Windows-1250")) %>% select(c(-2, -3)) %>% mutate(Leto=parse_number(Leto))
 
 tabela3 <- read_csv2("podatki/st_diplomantov_po_regijah.csv", skip = 1, col_names = c("Regija", "Leto", "Število diplomantov"),
                      locale=locale(encoding="Windows-1250"))
@@ -19,4 +19,11 @@ tabela4 <- read_csv2("podatki/st_diplomantov_po_vrsti_izobraževanja_spolu.csv",
                      locale=locale(encoding="Windows-1250")) %>% drop_na(3)
 
 tabela5 <- read_csv2("podatki/st_studentov_po_vrsti_izobrazevanja_nacinu_studija.csv", skip = 1,na = c("-"), col_names = c("Vrsta izobraževanja", "Leto", "Redni študij", "Izredni študij"),
-                     locale=locale(encoding="Windows-1250"))%>% drop_na(3)
+                     locale=locale(encoding="Windows-1250"))%>% drop_na(3) %>% mutate(Leto=parse_number(Leto))
+
+tabela13 <- tabela1 %>%
+  mutate(Leto=parse_number(Leto), # leta pretvorimo v števila
+         Regija=gsub("^stalno", "Stalno", Regija)) %>% # stalno -> Stalno na začetku
+  full_join(tabela3) %>% # združimo po skupnih stolpcih in ohranimo vse vrstice
+  filter(Regija != "SLOVENIJA") %>% # odstranimo sumarne podatke
+  gather(key="Kategorija", value="Stevilo", -Regija, -Leto) # pretvorimo v tidy data
