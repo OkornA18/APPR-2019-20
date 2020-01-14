eeb184b74c810183a30fb15ed1a93ea67219c32e
Ana Marija Okorn <anamarija.okorn@gmail.com>
2020-01-14 10:12:39 +0100

    dodana grafa(graf1 in graf2)

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,16 +1,16 @@
 # 3. faza: Vizualizacija podatkov
 
 # Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
-zemljevid <- fortify(zemljevid)
+#zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
+#                             pot.zemljevida="OB", encoding="Windows-1250")
+#levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
+#  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
+#zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
+#zemljevid <- fortify(zemljevid)
 
 # Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+#povprecja <- druzine %>% group_by(obcina) %>%
+#  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
 
 povprecje.dijakovinstudentov.po.regijah <- tabela1nova %>% group_by(regija,kategorija) %>% summarise(povprecje=sum(stevilo)/10)
 seznam <- split(povprecje.dijakovinstudentov.po.regijah, povprecje.dijakovinstudentov.po.regijah$kategorija)
@@ -18,3 +18,5 @@ povprecje.dijakov.po.regijah <- seznam[[1]]
 povprecje.studentov.po.regijah <- seznam[[2]]
 povprecje.dijakov.po.regijah <- povprecje.dijakov.po.regijah[-12,]
 povprecje.studentov.po.regijah <- povprecje.studentov.po.regijah[-12,]
+
+#graf1 <- ggplot(data = povprecje.dijakov.po.regijah,aes(x=leto, y=stevilo)) + geom_line(col="blue")+ ggtitle("Število zasedenih mest v zadnjem destletju")
